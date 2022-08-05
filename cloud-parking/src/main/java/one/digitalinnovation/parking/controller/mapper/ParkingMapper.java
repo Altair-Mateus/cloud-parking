dfed79f1a2a7e854ff38c9ab5ad24061a628ddfd
Altair-Mateus <taicoalencastro@gmail.com>
2022-08-04 21:48:05 -0300

    Criado o método findById() e implementado o @PostMapping

--- cloud-parking/src/main/java/one/digitalinnovation/parking/controller/mapper/ParkingMapper.java
+++ cloud-parking/src/main/java/one/digitalinnovation/parking/controller/mapper/ParkingMapper.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.parking.controller.mapper;
 
+import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
 import one.digitalinnovation.parking.controller.dto.ParkingDTO;
 import one.digitalinnovation.parking.model.Parking;
 import org.modelmapper.ModelMapper;
@@ -22,5 +23,16 @@ public class ParkingMapper {
 
         return parkingList.stream().map(this::toParkingDTO).collect(Collectors.toList());
 
+    }
+
+    public Parking toParking(ParkingDTO dto)
+    {
+        return MODEL_MAPPER.map(dto, Parking.class);
+
+    }
+
+    public Parking toParkingCreate(ParkingCreateDTO dto)
+    {
+        return MODEL_MAPPER.map(dto, Parking.class);
     }
 }
--- cloud-parking/src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ cloud-parking/src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,8 +18,11 @@ public class ParkingService {
     static
     {
         var id  = getUUID();
+        var id1  = getUUID();
         Parking parking = new Parking(id, "ABC-000", "RS", "oNIX", "BRANCO");
+        Parking parking1 = new Parking(id1, "ABC-123", "RS", "oNIX", "BRANCO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll()
@@ -31,4 +35,21 @@ public class ParkingService {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
+    public Parking findById(String id)
+    {
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate)
+    {
+        String uuid = getUUID();
+
+        parkingCreate.setId(getUUID());
+
+        parkingCreate.setEntryDate(LocalDateTime.now());
+
+        parkingMap.put(uuid, parkingCreate);
+
+        return parkingCreate;
+    }
 }
